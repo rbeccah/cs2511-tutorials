77e318971c9b10f6b5aa2489398bbeebf9daab87
Rebecca Hsu <rbeccahsu@gmail.com>
2024-10-31 00:49:51 +1100

    Update Tute07 starter code

--- tute07/src/thrones/decorator/ChainMail.java
+++ tute07/src/thrones/decorator/ChainMail.java
@@ -5,15 +5,6 @@ import thrones.Character;
 /*
  * Concrete decorator
  */
-public class ChainMail extends CharacterDecorator {
-
-    public ChainMail(Character wrappee) {
-        super(wrappee);
-    }
-
-    @Override
-    public void damage(int points) {
-        super.damage(points / 2);
-    }
+public class ChainMail {
     
 }
--- tute07/src/thrones/decorator/CharacterDecorator.java
+++ tute07/src/thrones/decorator/CharacterDecorator.java
@@ -8,53 +8,6 @@ import thrones.MoveResult;
 /*
  * Base Decorator class which all decorators inherit from
  */
-public class CharacterDecorator implements Character {
-    private Character wrappee;
-
-    public CharacterDecorator(Character wrappee) {
-        this.wrappee = wrappee;
-    }
-
-    @Override
-    public int getHealthPoints() {
-        return wrappee.getHealthPoints();
-    }
-
-    @Override
-    public int getX() {
-        return wrappee.getX();
-    }
-
-    @Override
-    public int getY() {
-        return wrappee.getY();
-    }
-
-    @Override
-    public void setX(int x) {
-        wrappee.setX(x);
-    }
-
-    @Override
-    public void setY(int y) {
-        wrappee.setY(y);
-    }
-
-    // * STEP 2: Calls damage() in the wrappee class (Character)
-    @Override
-    public void damage(int points) {
-        wrappee.damage(points);
-    }
-
-    @Override
-    public void attack(Character victim) {
-       wrappee.attack(victim);
-    }
-
-    @Override
-    public boolean canMove(int dx, int dy) {
-       return wrappee.canMove(dx, dy);
-    }
-
+public class CharacterDecorator {
 
 }
