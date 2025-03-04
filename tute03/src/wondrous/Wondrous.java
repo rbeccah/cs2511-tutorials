5719c6dff18ea5b7c727db30ff221ff5ede74c99
Rebecca Hsu <rbeccahsu@gmail.com>
2025-03-04 16:00:09 +1100

    Tute02

--- tute03/src/wondrous/Wondrous.java
+++ tute03/src/wondrous/Wondrous.java
@@ -8,19 +8,32 @@ public class Wondrous {
     private final int MY_MAGIC_NUMBER = 42;
 
     public List<Integer> wondrous(int start) {
+
+        if (start == 1) {
+            return new ArrayList<>();
+        } else if (start < 1) {
+            throw new IllegalArgumentException("wondrous must have start >= 1");
+        }
         int current = start;
         List<Integer> sequence = new ArrayList<Integer>();
 
         while (current != 1) {
             sequence.add(current);
+            test();
             if (current % 2 == 0) {
                 current /= 2;
             } else {
                 current = (current * 3) + 1;
             }
         }
+        sequence.add(current);
 
         return sequence;
+    }
+
+    public void test() {
+        int var = 0;
+        System.out.println(var);
     }
 
 }
