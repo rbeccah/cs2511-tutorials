5ece3f117251dc79793189a5cf651f471c6c9dcc
Rebecca Hsu <rbeccahsu@gmail.com>
2025-03-04 14:58:07 +1100

    Tute02

--- tute03/src/wondrous/Wondrous.java
+++ tute03/src/wondrous/Wondrous.java
@@ -8,19 +8,31 @@ public class Wondrous {
     private final int MY_MAGIC_NUMBER = 42;
 
     public List<Integer> wondrous(int start) {
+        if (start == 1) {
+            return new ArrayList<>();
+        }
         int current = start;
         List<Integer> sequence = new ArrayList<Integer>();
 
         while (current != 1) {
             sequence.add(current);
+            usingStackInDebugger();
             if (current % 2 == 0) {
                 current /= 2;
             } else {
                 current = (current * 3) + 1;
             }
+            usingStackInDebugger();
         }
 
+        sequence.add(1);
+
         return sequence;
+    }
+
+    public void usingStackInDebugger() {
+        int var = 10;
+        System.out.println("In stack debugger function");
     }
 
 }
--- tute03/src/wondrous/test/WondrousTest.java
+++ tute03/src/wondrous/test/WondrousTest.java
@@ -1,5 +1,6 @@
 package wondrous.test;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
@@ -17,5 +18,21 @@ public class WondrousTest {
         List<Integer> expected = new ArrayList<Integer>(Arrays.asList(3, 10, 5, 16, 8, 4, 2, 1));
         
         assertEquals(expected, w.wondrous(3));
+    }
+
+    @Test
+    public void testOne() {
+        Wondrous w = new Wondrous();
+
+        List<Integer> expected = new ArrayList<Integer>();
+        assertEquals(expected, w.wondrous(1));
+    }
+
+    @Test
+    public void testInvalid() {
+        Wondrous w = new Wondrous();
+        assertThrows(IllegalArgumentException.class, () -> {
+            w.wondrous(0);
+        });
     }
 }
