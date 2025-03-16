57dc4ecb2f0ff5fe8f7eb7ac862347ef92e024a0
Rebecca Hsu <rbeccahsu@gmail.com>
2025-03-16 22:08:55 +1100

    Update tute05 with more starter code

--- tute05/src/youtube/state/LockedState.java
+++ tute05/src/youtube/state/LockedState.java
@@ -3,34 +3,39 @@ package youtube.state;
 import youtube.VideoPlayer;
 
 public class LockedState extends ViewingState {
-
     public LockedState(VideoPlayer player) {
         super(player);
     }
 
     /*
-     * onLock() in the LockedState will unlock the video player. There are two cases:
-     *  - If the video is playing, it will go to PlayingState
-     *  - If video is not playing, it will go to LockedState
+     * onLock() in LockedState will unlock the video player. There are 2 cases:
+     *  - If video is playing, it will go to PlayingState
+     *  - If no video playing, it will go to ReadyState
      */
     @Override
     public void onLock() {
-
+        VideoPlayer player = getVideoPlayer();
+        if (player.getIsPlaying()) {
+            player.changeState(new PlayingState(player));
+            System.out.println("Unlock playing video");
+        } else {
+            player.changeState(new ReadyState(player));
+            System.out.println("Ready to play video");
+        }
     }
 
     @Override
     public void onPlay() {
-
+        System.out.println("Error: Cannot play the current video because it is locked");
     }
 
     @Override
     public void onNext() {
-        
+        System.out.println("Error: Cannot play the next video because it is locked");
     }
 
     @Override
     public String reportState() {
-        return null;
+        return "LockedState";
     }
-    
 }
--- tute05/src/youtube/state/PlayingState.java
+++ tute05/src/youtube/state/PlayingState.java
@@ -1,16 +1,18 @@
 package youtube.state;
 
+import youtube.Video;
 import youtube.VideoPlayer;
 
 public class PlayingState extends ViewingState {
-
     public PlayingState(VideoPlayer player) {
         super(player);
     }
 
     @Override
     public void onLock() {
-
+        VideoPlayer player = getVideoPlayer();
+        player.changeState(new LockedState(player));
+        System.out.println("Locking the video player in playing");
     }
 
     /*
@@ -18,17 +20,20 @@ public class PlayingState extends ViewingState {
      */
     @Override
     public void onPlay() {
-
+        VideoPlayer player = getVideoPlayer();
+        player.changeState(new ReadyState(player));
+        System.out.println("Pausing the video player");
     }
 
     @Override
     public void onNext() {
-
+        VideoPlayer player = getVideoPlayer();
+        player.changeState(new PlayingState(player));
+        System.out.println(player.playNextVideo());
     }
 
     @Override
     public String reportState() {
-        return null;
+        return "PlayingState";
     }
-    
 }
