5b20a35bee3b022d15e7257078122f6f7d10fa01
Rebecca Hsu <rbeccahsu@gmail.com>
2025-04-08 15:47:21 +1000

    Tute08

--- tute08/src/heist/BankAccountAccessor.java
+++ tute08/src/heist/BankAccountAccessor.java
@@ -9,11 +9,25 @@ public class BankAccountAccessor {
     
     private BankAccount account;
 
-    public BankAccountAccessor(BankAccount account) {
+    private static BankAccountAccessor instance;
+
+    private BankAccountAccessor(BankAccount account) {
         this.account = account;
     }
 
-    public void withdraw(String user, int numberOfWithdrawals, int amountPerWithdrawal) {
+    // Creation method
+    // * synchronized: only have one thread access this piece of code (scope) at any one time
+    // similar to locks
+    // ? Why might synchronized be bad? 
+    // Lost of the benefit of multiple threads, it can slow down the program
+    public synchronized static BankAccountAccessor getInstance(BankAccount account) {
+        if (instance == null) {
+            instance = new BankAccountAccessor(account);
+        }
+        return instance;
+    }
+
+    public synchronized void withdraw(String user, int numberOfWithdrawals, int amountPerWithdrawal) {
         System.out.println(user + " is accessing the bank.");
 
         for (int i = 0; i < numberOfWithdrawals; i++) {
--- tute08/src/stack/Stack.java
+++ tute08/src/stack/Stack.java
@@ -12,26 +12,37 @@ import java.util.List;
  */
 
 // ? What is E
+// E is a generic type which will be replaced by a concrete type when an instance of Stack is created
 
 // ? What is an Iterable interface?
+// List<String> stringList = new ArrayList<>();
+// for (String element : stringList) {
+// }
+// Is an interface that collections/classes can implement which allows us to iterate over those classes
+// Stack class can be used in for-each loops
 
 // ? Why does Iterable have E as well? 
+// So that Iterable can also take in the same type as our Stack
 
 // ? What methods does it force us to implement?
+// Since Stack is implementing Iterable, it forces us to implement .iterator() method
 
 public class Stack<E> implements Iterable<E> {
+    private List<E> elements = new ArrayList<E>();
     
     /**
      * Pushes an element onto the top of the stack.
      * @param element
      */
-    public void push(E element) {}
+    public void push(E element) {
+        elements.add(element);
+    }
 
     /**
      * Returns the top element of the stack, without removing it.
      */
     public E peek() {
-        return null;
+        return elements.get(elements.size() - 1);
     }
 
     /**
@@ -39,38 +50,68 @@ public class Stack<E> implements Iterable<E> {
      * @precondition The stack is not empty.
      */
     public E pop() {
-        return null;
+        E top = peek();
+        elements.remove(elements.size() - 1);
+        return top;
     }
 
     /**
      * Returns the stack as an ArrayList
      */
     public ArrayList<E> toArrayList() {
-        return null;
+        // ! What is not desirable about this implementation? 
+        // Breaking encapsulation because Java will return Stack's own private elements (same place in memory)
+        // User can manipualte the underlying list in ways that violate the principle of Stack
+        // return (ArrayList<E>) elements;
+
+        // Instead we create a copy of the elements
+        return new ArrayList<E>(elements);
     }
 
     /**
      * Returns an iterator to the internal data structure of the stack.
      */
     public Iterator<E> iterator() {
-        return null;
+        List<E> copy = toArrayList();
+        Collections.reverse(copy);
+
+        return copy.iterator();
     }
 
     /**
      * Returns the size of the stack.
      */
     public int size() {
-        return 0;
+        return elements.size();
     }
 
     // ? What does <? extends Integer> mean?
+    // wildcard
+    // This method accepts parameters that are a Stack of elements of any type which is either Integer or subclass of Integer
+    // Upper bound
 
     // ? Why don't we just do Stack<Integer>?
+    // For flexibility
+    // We can make our own custom class MyInteger, and it can also be accepted
 
     // ? What is the difference between `?` and E?
+    // Like ? first = copy.get(0); <- doesnt work
+    //// E first = copy.get(0);
 
     public static Integer sumStack(Stack<? extends Integer> stack) {
-        return 0;
+        // int total = 0;
+        // for (Integer element : stack) {
+        //     total += element;
+        // }
+        // return total;
+
+        // Using iterator methods
+        int total = 0;
+        Iterator<? extends Integer> iter = stack.iterator();
+        while (iter.hasNext()) {
+            total += iter.next();
+        }
+        return total;
     }
 
     /**
@@ -78,7 +119,17 @@ public class Stack<E> implements Iterable<E> {
      * "[1, 2, 3]"
      * @param stack
      */
-    public static void prettyPrint(Stack<?> stack) {}
+    public static void prettyPrint(Stack<?> stack) {
+        System.out.print("[");
+        Iterator<?> iter = stack.iterator();
+        while (iter.hasNext()) {
+            System.out.print(iter.next());
+            if (iter.hasNext()) {
+                System.out.print(",");
+            }
+        }
+        System.out.println("]");
+    }
         
 
     public static void main(String[] args) {
@@ -89,6 +140,7 @@ public class Stack<E> implements Iterable<E> {
         stack.push("you");
         stack.push("today");
         prettyPrint(stack);
+        // System.out.println(Stack.sumStack(stack));
 
         Stack<Integer> stack2 = new Stack<Integer>();
         stack2.push(2);
